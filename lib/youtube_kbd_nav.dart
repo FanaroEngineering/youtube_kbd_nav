0de982c3359e4c20c491e27f394045689c98b221
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-04 12:17:22 -0300

    refactoring

--- lib/youtube_kbd_nav.dart
+++ lib/youtube_kbd_nav.dart
@@ -5,25 +5,30 @@ import 'package:meta/meta.dart';
 
 class YoutubeMainPage {
   final Document _document;
+  List<Element> _thumbnails;
   int _currentThumbnailIndex = -1;
 
   YoutubeMainPage({
     @required Document input,
-  }) : _document = input;
+  }) : _document = input {
+    _thumbnails = _document.querySelectorAll('ytd-rich-item-renderer');
+  }
+
+  Element get _thumbnailFromCurrentIndex => _thumbnails[_currentThumbnailIndex];
 
   void addBorderToNext() {
+    _deleteCurrentThumbnailCss();
     final Element currentThumbnail = _selectNextThumbnail();
     currentThumbnail.attributes['style'] = _css;
   }
 
+  void _deleteCurrentThumbnailCss() => _currentThumbnailIndex >= 0
+      ? _thumbnailFromCurrentIndex.attributes.remove('style')
+      : null;
+
   Element _selectNextThumbnail() {
-    final List<Element> thumbnails =
-        _document.querySelectorAll('ytd-rich-item-renderer');
-    if (_currentThumbnailIndex >= 0) {
-      thumbnails[_currentThumbnailIndex].attributes.remove('style');
-    }
     _currentThumbnailIndex++;
-    return thumbnails[_currentThumbnailIndex];
+    return _thumbnailFromCurrentIndex;
   }
 }
 
