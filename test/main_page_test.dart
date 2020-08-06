86cb782c8fcacdc4ef48fd4d715d64b8c15bbd6c
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-06 16:37:15 -0300

    a little bit more refactoring | fixes #4

--- test/main_page_test.dart
+++ test/main_page_test.dart
@@ -9,13 +9,13 @@ void main() {
     document.body.append(ParagraphElement());
     document.body.append(ParagraphElement());
 
-    MainPage youtubeMainPage;
+    MainPage mainPage;
 
-    setUp(() => youtubeMainPage = MainPage(input: document, tag: 'p'));
+    setUp(() => mainPage = MainPage(input: document, tag: 'p'));
 
     group('Moving across thumbnails', () {
       test('Puts border on the first thumbnail', () {
-        youtubeMainPage.addBorder(NextOrPrevious.next);
+        mainPage.addBorder(NextOrPrevious.next);
 
         final String newStyle = document.querySelector('p').attributes['style'];
 
@@ -25,8 +25,8 @@ void main() {
       test(
           'Putting a border on the second thumbnail deletes the border on the '
           'first', () {
-        youtubeMainPage.addBorder(NextOrPrevious.next);
-        youtubeMainPage.addBorder(NextOrPrevious.next);
+        mainPage.addBorder(NextOrPrevious.next);
+        mainPage.addBorder(NextOrPrevious.next);
 
         final List<Element> thumbnails = document.querySelectorAll('p');
         final Element firstThumbnail = thumbnails.first;
@@ -37,9 +37,9 @@ void main() {
       });
 
       test('Can also go to the previous thumbnail', () {
-        youtubeMainPage.addBorder(NextOrPrevious.next);
-        youtubeMainPage.addBorder(NextOrPrevious.next);
-        youtubeMainPage.addBorder(NextOrPrevious.previous);
+        mainPage.addBorder(NextOrPrevious.next);
+        mainPage.addBorder(NextOrPrevious.next);
+        mainPage.addBorder(NextOrPrevious.previous);
 
         final List<Element> thumbnails = document.querySelectorAll('p');
         final Element firstThumbnail = thumbnails.first;
@@ -61,9 +61,7 @@ void main() {
 
       MainPage youtubeMainPage;
 
-      setUp(() {
-        youtubeMainPage = MainPage(input: document, tag: 'div');
-      });
+      setUp(() => youtubeMainPage = MainPage(input: document, tag: 'div'));
 
       test('Extracts the link to the thumbnail', () {
         youtubeMainPage.addBorder(NextOrPrevious.next);
