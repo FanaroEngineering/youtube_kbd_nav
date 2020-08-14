aa445fa93d11db5efdd08e8fc15a7c2c17a2a978
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-14 18:34:13 -0300

    implemented the logic for the cycler

--- lib/src/cycler.dart
+++ lib/src/cycler.dart
@@ -1,1 +1,9 @@
-class Cycler {}
+class Cycler {
+  int _index = -1;
+
+  int get index => _index;
+
+  void forward() => _index++;
+
+  void backward() => _index <= 0 ? null : _index--;
+}
--- test/page_test.dart
+++ test/page_test.dart
@@ -1,13 +1,47 @@
-import 'dart:html';
-
 import 'package:test/test.dart';
 
 import 'package:youtube_kbd_nav/youtube_kbd_nav.dart';
 
 void main() {
   group('Cycler\'s Logic |', () {
-    test('', () {
-      
+    Cycler cycler;
+
+    setUp(() => cycler = Cycler());
+
+    test('Index starts at -1', () {
+      expect(cycler.index, -1);
+    });
+
+    test('Going forward adds to the index', () {
+      cycler.forward();
+
+      expect(cycler.index, 0);
+
+      cycler.forward();
+
+      expect(cycler.index, 1);
+    });
+
+    test('Cannot go back -1', () {
+      cycler.backward();
+
+      expect(cycler.index, -1);
+    });
+
+    test('Cannot go back 0 after having added', () {
+      cycler.forward();
+      cycler.backward();
+
+      expect(cycler.index, 0);
+    });
+
+    test('Going forward 3 times and then backward returns 1', () {
+      cycler.forward();
+      cycler.forward();
+      cycler.forward();
+      cycler.backward();
+
+      expect(cycler.index, 1);
     });
   });
 }
