881b381dcc99a157371fc8ce36232cde60c287c3
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-14 17:53:02 -0300

    clean slate

--- test/page_test.dart
+++ test/page_test.dart
@@ -2,74 +2,12 @@ import 'dart:html';
 
 import 'package:test/test.dart';
 
-import 'package:youtube_kbd_nav/export.dart';
+import 'package:youtube_kbd_nav/youtube_kbd_nav.dart';
 
 void main() {
-  group('Main Page Thumbnails Navigation |', () {
-    document.body.append(ParagraphElement());
-    document.body.append(ParagraphElement());
-
-    Page mainPage;
-
-    setUp(() => mainPage = Page(input: document, tag: 'p'));
-
-    group('Moving across thumbnails', () {
-      test('Puts border on the first thumbnail', () {
-        mainPage.addBorder(NextOrPrevious.next);
-
-        final String newStyle = document.querySelector('p').attributes['style'];
-
-        expect(newStyle, isNotNull);
-      });
-
-      test(
-          'Putting a border on the second thumbnail deletes the border on the '
-          'first', () {
-        mainPage.addBorder(NextOrPrevious.next);
-        mainPage.addBorder(NextOrPrevious.next);
-
-        final List<Element> thumbnails = document.querySelectorAll('p');
-        final Element firstThumbnail = thumbnails.first;
-        final Element secondThumbnail = thumbnails.last;
-
-        expect(firstThumbnail.attributes['style'], isNull);
-        expect(secondThumbnail.attributes['style'], isNotNull);
-      });
-
-      test('Can also go to the previous thumbnail', () {
-        mainPage.addBorder(NextOrPrevious.next);
-        mainPage.addBorder(NextOrPrevious.next);
-        mainPage.addBorder(NextOrPrevious.previous);
-
-        final List<Element> thumbnails = document.querySelectorAll('p');
-        final Element firstThumbnail = thumbnails.first;
-        final Element secondThumbnail = thumbnails.last;
-
-        expect(firstThumbnail.attributes['style'], isNotNull);
-        expect(secondThumbnail.attributes['style'], isNull);
-      });
-    });
-
-    group('Action on Thumbnail |', () {
-      final String correctUrl = 'https://hello.com';
-
-      document.body.append(DivElement());
-      document.body.querySelector('div').append(DivElement());
-      document.body
-          .querySelector('div > div')
-          .append(AnchorElement()..href = correctUrl);
-
-      Page youtubeMainPage;
-
-      setUp(() => youtubeMainPage = Page(input: document, tag: 'div'));
-
-      test('Extracts the link to the thumbnail', () {
-        youtubeMainPage.addBorder(NextOrPrevious.next);
-
-        final String extractedLink = youtubeMainPage.currentThumbnailLink;
-
-        expect(extractedLink, correctUrl);
-      });
+  group('Cycler\'s Logic |', () {
+    test('', () {
+      
     });
   });
 }
--- web/content.dart
+++ web/content.dart
@@ -1,57 +1,5 @@
 import 'dart:html';
 
-import 'package:youtube_kbd_nav/export.dart';
+import 'package:youtube_kbd_nav/youtube_kbd_nav.dart';
 
-void main() {
-  // Page page;
-
-  document.addEventListener('yt-navigate-finish', (Event event) {
-    document.querySelectorAll(
-        'ytd-rich-item-renderer, ytd-compact-video-renderer, ytd-compact-radio-renderer')
-      ..forEach((Element element) => element.attributes.remove('style'));
-    //   print(document.baseUri);
-
-    //   // page = Page(input: document);
-
-    //   // page = Page(
-    //   //         input: document,
-    //   //         tag: 'ytd-compact-video-renderer, ytd-compact-radio-renderer');
-
-    //   page = document.baseUri.contains('watch')
-    //       ? Page(
-    //           input: document,
-    //           tag: 'ytd-compact-video-renderer, ytd-compact-radio-renderer')
-    //       : Page(input: document);
-  });
-
-  int index = 0;
-  document.onKeyPress.listen((KeyboardEvent keyboardEvent) {
-    // final Page page = Page(input: document);
-    // final Page page = Page(
-    //         input: document,
-    //         tag: 'ytd-compact-video-renderer, ytd-compact-radio-renderer');
-    final Page page = document.baseUri.contains('watch')
-        ? Page(
-            input: document,
-            tag: 'ytd-compact-video-renderer, ytd-compact-radio-renderer',
-            index: index)
-        : Page(input: document, index: index);
-
-    print(document.baseUri);
-
-    switch (keyboardEvent.key) {
-      case 'z':
-        page?.addBorder(NextOrPrevious.next);
-        index++;
-        break;
-      case 'x':
-        page?.addBorder(NextOrPrevious.previous);
-        index--;
-        break;
-      case 'Enter':
-        final String thumbnailLink = page?.currentThumbnailLink;
-        thumbnailLink != null ? window.open(thumbnailLink, '') : null;
-        break;
-    }
-  });
-}
+void main() {}
