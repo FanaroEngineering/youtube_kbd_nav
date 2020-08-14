cc607b7d2ff691472c49e8e0bcebfebdb2241304
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-14 18:39:07 -0300

    put the cycler on the content script

--- web/content.dart
+++ web/content.dart
@@ -2,4 +2,17 @@ import 'dart:html';
 
 import 'package:youtube_kbd_nav/youtube_kbd_nav.dart';
 
-void main() {}
+void main() {
+  final Cycler cycler = Cycler();
+
+  document.onKeyPress.listen((KeyboardEvent keyboardEvent) {
+    switch (keyboardEvent.key) {
+      case 'z':
+        cycler.forward();
+        break;
+      case 'x':
+        cycler.backward();
+        break;
+    }
+  });
+}
