2f14e8d2f470ce517965348442632c53f7f24712
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-16 11:00:50 -0300

    trying to reset the UI when navigating to a new page

--- web/content.dart
+++ web/content.dart
@@ -1,9 +1,13 @@
-import 'dart:html' show document, KeyboardEvent;
+import 'dart:html' show document, KeyboardEvent, window, Event;
 
 import 'package:youtube_kbd_nav/youtube_kbd_nav.dart' show KbdHandler;
 
 void main() {
   final KbdHandler kbdHandler = KbdHandler();
+
+  window.addEventListener('yt-navigate-start', (Event event) {
+    
+  });
 
   document.onKeyPress.listen((KeyboardEvent keyboardEvent) =>
       kbdHandler.onKeyPress(keyboardEvent, newUrl: document.baseUri));
