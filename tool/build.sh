aefef483c8037673b0e3b1960f862ad431cd918d
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-19 16:05:19 -0300

    added a script for building the extension on UNIX systems | fixes #24

---
+++ tool/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$BUILD_DIR="build"
+
+if [[ ! -e $BUILD_DIR ]]; then
+    mkdir $BUILD_DIR
+fi
+
+dart2js --csp -O4 -o build/content.dart.js web/content.dart
+
+cp web/manifest.json build
+
+zip -r build.zip build 
