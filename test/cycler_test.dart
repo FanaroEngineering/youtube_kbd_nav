4f41a7728043a0b9ab45a5999fafeec57efab9f3
Philippe Fanaro <philippefanaro@gmail.com>
2020-08-28 21:57:39 -0300

    less stuff on the cycler's tests

--- test/cycler_test.dart
+++ test/cycler_test.dart
@@ -1,47 +1,43 @@
-import 'package:test/test.dart' show expect, group, setUp, test;
+import 'package:test/test.dart' show expect, setUp, test;
 
 import 'package:youtube_kbd_nav/youtube_kbd_nav.dart' show Cycler;
 
 void main() {
-  group('Cycler\'s Logic |', () {
-    Cycler cycler;
+  Cycler cycler;
 
-    setUp(() => cycler = Cycler());
+  setUp(() => cycler = Cycler());
 
-    test('Index starts at -1', () {
-      expect(cycler.total, -1);
-    });
+  test('Index starts at -1', () => expect(cycler.total, -1));
 
-    test('Going forward adds to the index', () {
-      cycler.forwards();
+  test('Going forward adds to the index', () {
+    cycler.forwards();
 
-      expect(cycler.total, 0);
+    expect(cycler.total, 0);
 
-      cycler.forwards();
+    cycler.forwards();
 
-      expect(cycler.total, 1);
-    });
+    expect(cycler.total, 1);
+  });
 
-    test('Cannot go back -1', () {
-      cycler.backwards();
+  test('Cannot go back -1', () {
+    cycler.backwards();
 
-      expect(cycler.total, -1);
-    });
+    expect(cycler.total, -1);
+  });
 
-    test('Cannot go back 0 after having added', () {
-      cycler.forwards();
-      cycler.backwards();
+  test('Cannot go back 0 after having added', () {
+    cycler.forwards();
+    cycler.backwards();
 
-      expect(cycler.total, 0);
-    });
+    expect(cycler.total, 0);
+  });
 
-    test('Going forward 3 times and then backward returns 1', () {
-      cycler.forwards();
-      cycler.forwards();
-      cycler.forwards();
-      cycler.backwards();
+  test('Going forward 3 times and then backward returns 1', () {
+    cycler.forwards();
+    cycler.forwards();
+    cycler.forwards();
+    cycler.backwards();
 
-      expect(cycler.total, 1);
-    });
+    expect(cycler.total, 1);
   });
 }
