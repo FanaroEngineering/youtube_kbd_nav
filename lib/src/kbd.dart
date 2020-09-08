9c781e435db2ba4c577b3cb9b6c432379a787d23
Philippe Fanaro <philippefanaro@gmail.com>
2020-09-08 14:46:15 -0300

    greatly simplified the internals of the Kbd class | ref #13

--- lib/src/kbd.dart
+++ lib/src/kbd.dart
@@ -1,4 +1,4 @@
-import 'dart:html' show Document, document, Element, KeyboardEvent, window;
+import 'dart:html' show document, Element, KeyboardEvent, window;
 
 import 'cycler.dart' show Cycler;
 import 'thumbnails.dart' show Thumbnails;
@@ -6,52 +6,38 @@ import 'url_handler.dart' show UrlHandler;
 import 'video_buttons.dart' show VideoButtons;
 
 class Kbd {
-  Document _document;
-  Cycler _cycler;
-  String _url = '';
-  Thumbnails _thumbnails = Thumbnails(tags: UrlHandler.tags('watch'));
+  String _url = document.baseUri;
+  Cycler _cycler = Cycler();
+  Thumbnails _thumbnails = Thumbnails(tags: UrlHandler.tags(document.baseUri));
   VideoButtons _videoButtons = VideoButtons();
   KeyboardEvent _keyboardEvent;
 
-  /// The [Cycler] and [Document] are parameters just so we can inject them
-  /// during testing and check if everything happened as expected.
-  Kbd({Document doc}) : _document = doc ?? document {
+  Kbd() {
     window.addEventListener(
-        'yt-navigate-start', (_) => _resetStylesAndCycler());
-    _document.onKeyDown.listen((KeyboardEvent keyboardEvent) {
+        'yt-navigate-start', (_) => _resetStylesAndCyclerAndUrl());
+    document.body.onKeyDown.listen((KeyboardEvent keyboardEvent) {
       _keyboardEvent = keyboardEvent;
-      _onKeyDown(newUrl: _document.baseUri);
+      _keySwitch();
     });
   }
 
-  void _resetStylesAndCycler() {
-    _thumbnails?.resetCurrentThumbnail();
+  void _resetStylesAndCyclerAndUrl() {
+    _url = document.baseUri;
     _cycler = Cycler();
+    _thumbnails.resetCurrentThumbnail();
+    _thumbnails = Thumbnails(tags: UrlHandler.tags(_url));
   }
 
-  Future<void> _onKeyDown({String newUrl = ''}) async {
-    _resetCyclerIfUrlChange(newUrl);
-    await _keySwitch();
-  }
-
-  void _resetCyclerIfUrlChange(String newUrl) {
-    if (newUrl != _url) {
-      _url = newUrl;
-      _cycler = Cycler();
-    }
-  }
-
-  Element get _searchBar => _document.querySelector('input#search');
-  Element get _playlistName => _document.querySelector('iron-input > input');
-  Element get _commentBox => _document.querySelectorAll(_commentBoxQuery)[0];
-  Element get _editCommentBox =>
-      _document.querySelectorAll(_commentBoxQuery)[1];
+  Element get _searchBar => document.querySelector('input#search');
+  Element get _playlistName => document.querySelector('iron-input > input');
+  Element get _commentBox => document.querySelectorAll(_commentBoxQuery)[0];
+  Element get _editCommentBox => document.querySelectorAll(_commentBoxQuery)[1];
   String get _commentBoxQuery => 'yt-formatted-string.ytd-commentbox > div';
 
-  bool get _noInputFocus => !(_searchBar == _document.activeElement ||
-      _commentBox == _document.activeElement ||
-      _editCommentBox == _document.activeElement ||
-      _playlistName == _document.activeElement);
+  bool get _noInputFocus => !(_searchBar == document.activeElement ||
+      _commentBox == document.activeElement ||
+      _editCommentBox == document.activeElement ||
+      _playlistName == document.activeElement);
 
   Future<void> _keySwitch() async {
     if (_noInputFocus) {
@@ -68,9 +54,7 @@ class Kbd {
           _navigate(UrlHandler.youtubeHome);
           break;
         case 'Enter':
-          if (_cycler.total >= 0) {
-            _navigate(_thumbnails?.thumbnailLink);
-          }
+          if (_cycler.total >= 0) _navigate(_thumbnails?.thumbnailLink);
           break;
         case 'h':
           _navigate(UrlHandler.history);
@@ -84,28 +68,26 @@ class Kbd {
         case 'n':
           if (_isVideo) _videoButtons?.dislike();
           break;
+        case 'y':
+          _videoButtons?.notiticationPopUp();
+          break;
         case 'b':
           if (_isVideo)
             await window.navigator.clipboard
                 .writeText(UrlHandler.shortenLink(_url));
           break;
-        case 'y':
-          _videoButtons?.notiticationPopUp();
-          break;
-        default:
       }
     }
   }
 
-  bool get _isVideo => _url.contains('watch');
-
+  // This is necessary because the thumbnails won't be fully loaded when
+  // `yt-navigate-start`.
   void _addBorder() {
-    if (_url != null) {
-      final String tags = UrlHandler.tags(_url);
-      _thumbnails = Thumbnails(tags: tags);
-      _thumbnails.addBorder(index: _cycler.total);
-    }
+    _thumbnails = Thumbnails(tags: UrlHandler.tags(_url));
+    _thumbnails.addBorder(index: _cycler.total);
   }
+
+  bool get _isVideo => _url.contains('watch');
 
   void _navigate(String url) {
     final bool modifierKey = window.navigator.appVersion.contains('Mac')
