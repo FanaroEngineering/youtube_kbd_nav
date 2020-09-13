cf31ef2df5fcdce09fe5edd993348d0266fd8ed7
Philippe Fanaro <philippefanaro@gmail.com>
2020-09-13 14:11:52 -0300

    refactoring improvement to the UrlHandler

--- lib/src/url_handler.dart
+++ lib/src/url_handler.dart
@@ -2,24 +2,23 @@ class UrlHandler {
   static const String youtubeHome = 'https://www.youtube.com';
   static const String history = 'https://www.youtube.com/feed/history';
 
-  static String tags(String url) {
-    if (url.contains('watch')) {
-      return 'ytd-compact-video-renderer, '
-          'ytd-compact-radio-renderer, '
-          'ytd-compact-playlist-renderer';
-    } else if (url.contains('history')) {
-      return 'ytd-video-renderer';
-    } else if (url.contains('results')) {
-      return 'ytd-video-renderer, '
-          'ytd-radio-renderer, '
-          'ytd-playlist-renderer, '
-          'ytd-channel-renderer';
-    } else if (url.contains('youtube')) {
-      return 'ytd-rich-item-renderer';
-    } else {
-      return 'p';
-    }
-  }
+  static const String _watchTags = 'ytd-compact-video-renderer, '
+      'ytd-compact-radio-renderer, '
+      'ytd-compact-playlist-renderer';
+  static const String _historyTags = 'ytd-video-renderer';
+  static const String _resultsTags = 'ytd-video-renderer, '
+      'ytd-radio-renderer, '
+      'ytd-playlist-renderer, '
+      'ytd-channel-renderer';
+  static const String _homeTags = 'ytd-rich-item-renderer';
+
+  static String tags(String url) => url.contains('watch')
+      ? _watchTags
+      : url.contains('history')
+          ? _historyTags
+          : url.contains('results')
+              ? _resultsTags
+              : url.contains('youtube') ? _homeTags : 'p';
 
   static String prefixedLink(String url) =>
       url.startsWith('https://') ? url : youtubeHome + url;
